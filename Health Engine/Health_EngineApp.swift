c1f28ef9f599754e32c64fe02e277816d593d941
K2 <ketupatel2000@gmail.com>
2026-07-29 10:54:53 -0400

    Initial Commit

---
+++ Health Engine/Health_EngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_EngineApp.swift
+//  Health Engine
+//
+//  Created by Sankalp on 2026-07-29.
+//
+
+import SwiftUI
+
+@main
+struct Health_EngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
